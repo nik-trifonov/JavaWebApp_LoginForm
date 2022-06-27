faf39c31c840438f9874326fd85e1ba07d1b5b24
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2022-06-27 20:58:26 +0300

    Add files via upload

---
+++ Java Web Login/src/java/controller/login.java
@@ -0,0 +1,49 @@
+
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS
+ */
+@WebServlet(name = "login", urlPatterns = {"/login"})
+public class login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            String _username = request.getParameter("username");
+            String _password = request.getParameter("password");
+            if (_username != null && _password != null) {
+                if (_username.equals("Ivan") && _password.equals("12345")) {
+                    response.sendRedirect("welcome.jsp");
+                } else {
+                    out.println("Invalid user or password");
+                }
+            } else {
+                     out.println("Empty username or password");
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
---
+++ Java Web Login/web/welcome.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : welcome
+    Created on : Jun 11, 2022, 12:03:22 PM
+    Author     : ASUS
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome</title>
+    </head>
+    <body>
+        <center><h1>Welcome</h1></center>
+        <style>
+            body{
+                background-image: url('img.jpg')
+            }
+            
+        </style>
+    </body>
+</html>
